1512540d606595d72ca9a5f55e3b19c3f1efe752
hadil kharroubi <hadilkharroubi85@gmail.com>
2025-11-03 22:53:41 +0100

    simple modif

---
+++ Downloads/musicapp/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { Logincomponent } from './logincomponent/logincomponent';
+import { Maincomponent } from './maincomponent/maincomponent';
+import { Homecomponenet } from './homecomponenet/homecomponenet';
+import { Library } from './library/library';
+import { Signin } from './signin/signin';
+
+export const routes: Routes = [{path:'login',component:Logincomponent},{path:'search',component:Maincomponent},{path:'home',component:Homecomponenet}
+    ,{path:'library',component:Library},{path:'sign',component:Signin}
+];
---
+++ Downloads/musicapp/src/app/maincomponent/maincomponent.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-maincomponent',
+  imports: [],
+  templateUrl: './maincomponent.html',
+  styleUrl: './maincomponent.scss'
+})
+export class Maincomponent {
+
+}
